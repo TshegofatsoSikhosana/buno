d52e46e125b8e569f7985f0c6a56c328496eea00
Tshegofatso Sikhosana <ts98sikhosana@gmail.com>
2025-12-09 21:40:47 +0200

    updated to account for december to jan cloning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,6 +23,7 @@ import Dashboard from '@/components/dashboard/Dashboard';
 import { Tab, isTabActive } from '@/model/shared';
 import ShareModal from '@/components/share/ShareModal';
 import Link from 'next/link';
+import CarLoan from '@/components/car-loan/CarLoan';
 
 
 export default function Home() {
@@ -52,6 +53,8 @@ export default function Home() {
         return <Groceries />
       case Tab.DASHBOARD:
         return <Dashboard />
+      case Tab.CAR_LOAN:
+        return <CarLoan />
       default:
         return <Expenses setTotalExpenses={setTotalExpenses} />
     }
@@ -211,6 +214,13 @@ export default function Home() {
             onClick={(e)=> setActive(Tab.INCOME)}
            >
               Income
+          </div>
+          <div 
+           className={`inline-block p-4 ${isTabActive(Tab.CAR_LOAN, active)}`}
+           onClick={(e)=> setActive(Tab.CAR_LOAN)}
+           > 
+            {/* <div className='inline-block p-1'><Icon svgPath={reportSvg} onClick={() => {}}/> </div>  */}
+            <div className='inline-block'>Car Loan</div>
           </div>
           <div 
            className={`inline-block p-4 ${isTabActive(Tab.DASHBOARD, active)}`}
